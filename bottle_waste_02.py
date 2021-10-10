d723041bcfffaee50d15094072c10f40007ac6f1
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-10 15:56:05 +0330

    repair a ring area defects(ring crack)

---
+++ bottle_waste_02.py
@@ -0,0 +1,172 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair the ring crack below the branch area defects branch 
+print("what is ring crack?")
+print("a ring that has a thin crack in the threads or in the middle is called a ring crack")
+# how to diagnose it
+print("how to diagnose it ?")
+print("it is usually visible by turning the bottle in front of the light and reflecting")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+melt = input("is the melt too cold?    [yes/no]=>")
+if melt == "yes":
+    print("for fix it you have to adjust the bite temperature ")
+elif melt == "no":
+    print("okay answer this question")
+    scissors = input("is there a scissor line?   [yes/no]=>")
+    if scissors == "yes":
+        print("okay for fix it you have to replace the scissor blades")
+    elif scissors == "no":
+        print("okay answer this question")
+        bite_round = input("is the bite too round?    [yes/no]=>")
+        if bite_round == "yes":
+            print("for fix it correct the shape of the bite")
+        elif bite_round == "no":
+            print("answer this question")
+            # here is about operator and machion setting
+            print("here is about operator and machion setting")
+            arm_of_the_ring = input("is the arm of the ring set too low on the mold so that when closing the mold, it causes the rim holder to move upwards due to contact with the mold?   [yes/no]=>")
+            if arm_of_the_ring == "yes":
+                print("for fix it you have to adjust the ring arms with the corresponding gauge")
+            elif arm_of_the_ring == "no":
+                print("okay answer this question")
+                ring_mechanism = input("is the height of the arm ring on the side of the blink not adjusted properly so that when the blink is opened, it causes movement and pressure on the ring?    [yes/no]=>") 
+                if ring_mechanism == "yes":
+                    print("for fix it you have to adjust the reversing mechanism with the corresponding fixture")
+                elif ring_mechanism == "no":
+                    print("okay answer this question")
+                    ring_arms_holder = input("is the rim arm not fully angled?   [yes/no]=>")
+                    if ring_arms_holder == "yes":
+                        print("okay for fox ot you have to change ring arms holder")
+                    elif ring_arms_holder == "no":
+                        print("okay answer this question")
+                        inflator_blink = input("is the time or pressure of applying Blink air inflator high?     [yes/no]=>")
+                        if inflator_blink == "yes":
+                            print("for fix it you have to reduce the pressure or air inflators time of the blink")
+                        elif inflator_blink == "no":
+                            print("answer this question")
+                            plunger_cooling = input("head ring or plunger is too cold")
+                            if plunger_cooling == "yes":
+                                print("for fix it you have to reduce the time or cooling air pressure of the plunger ")
+                            elif plunger_cooling == "no":
+                                print("okay answerthis question")
+                                shock_absorber = input("does the end-of-course shock absorber mechanism not work properly?     [yes/no]=>")
+                                if shock_absorber == "yes": 
+                                    print("okay for fix it check the bumper mechanism of the reversing ring")
+                                elif shock_absorber == "no":
+                                    print("okay answer this question")
+                                    p_and_b = input("is there too much cooling air for the rim in the P&B system?    [yes/no]=>")
+                                    if p_and_b == "yes":
+                                        print("for fix it reduce the cooling air")
+                                    if p_and_b == "no":
+                                        print("okay answer this question")
+                                        water_oil = input("is there a lot of water and oil spilling from the studs on the tip ring?    [yes/no]=>")
+                                        if water_oil == "yes":
+                                            print("for fix it you have to stop the water and oil spilling from the studs")
+                                        elif water_oil == "no":
+                                            print("okay answer this question")
+                                            oil = input("is there too much oil spraying?    [yes/no]=>")
+                                            if oil == "yes":
+                                                print("for fix it reduce oil spraying") 
+                                            elif oil == "no":
+                                                print("okay answer a question")
+                                                ring = input("doesn't the ring release easily when it is either dry or opens too fast or too slowly?   [yes/no]=>")
+                                                if ring == "yes":
+                                                    print("okay for fix it adjust the opening of the ring")
+                                                elif ring == "no":
+                                                    print("okay answer this question")
+                                                    height_mechanism = input("is the height of the plunger mechanism not adjusted correctly?   [yes/no]=>")
+                                                    if height_mechanism == "yes":
+                                                        print("for fix it measure the height of the plunger mechanism ")
+                                                    elif height_mechanism == "no":
+                                                        print("answer this question")
+                                                        baffle_mechanism = input("Is the intensity of the downward movement of the baffle mechanism so high that it causes pressure and bending of the blink?   [yes/no]=>")
+                                                        if baffle_mechanism == "yes":
+                                                            print("for fix it you have to adjust the movement speed of the baffle mechanism")
+                                                        elif baffle_mechanism == "no":
+                                                            print("answer this question")
+                                                            blink = input("is the housing of the ring in the blink is so dirty that when the blink is opened, it causes the tip ring to open?    [yes/no]=>")
+                                                            if blink == "yes":
+                                                                print("for fix it you have to change the blink")
+                                                            elif blink == "no":
+                                                                print("well answer this question")
+                                                                vacuum = input("is the vacuum announcement time long enough to cool the ring components?    [yes/no]=>")
+                                                                if vacuum == "yes":
+                                                                    print("for fix it reduce vacuum announcement time")
+                                                                elif vacuum == "no":
+                                                                    print("answer this question")
+                                                                    # here is about template components
+                                                                    print("here is about template components")
+                                                                    threads = input("is the edges of the tip ring are very sharp. The threads do not loosen easily?   [yes/no]=>")
+                                                                    if threads == "yes":
+                                                                        print("for fix it polish the tip ring threads")
+                                                                    elif threads == "no":
+                                                                        print("answer this question")
+                                                                        tip_ring = input("is the inside of the tip ring is not polished   [yes/no]=>")
+                                                                        if tip_ring == "yes":
+                                                                            print("polish inside inside of the tip ring")
+                                                                        elif tip_ring == "no":
+                                                                            print("answer this question")
+                                                                            ring_blink = input("is the ring and Blink are not compatible?    [yes/no]=>")
+                                                                            if ring_blink == "yes":
+                                                                                print("for fix it you have to use the right gauge")
+                                                                            elif ring_blink == "no":
+                                                                                print("answer this question")
+                                                                                ring_blink2 = input("is there no proper gap between the ring and the blink?   [yes/no]=>")
+                                                                                if ring_blink2 == "yes":
+                                                                                    print("create a suitable distance between the ring and the blink")
+                                                                                elif ring_blink2 == "no":
+                                                                                    print("answer the last question")
+                                                                                    size_of_the_tip_ring = input("is the size of the tip ring large?   [yes/no]=>")
+                                                                                    if size_of_the_tip_ring == "yes":
+                                                                                        print("for fix it you have to use a smaller ring")
+                                                                                    elif size_of_the_tip_ring == "no":
+                                                                                        print("i do not know your problem")
+                                                                                    else:
+                                                                                        print("incorrect")
+                                                                                else:
+                                                                                    print("incorrect")
+                                                                            else:
+                                                                                print("incorrect")
+                                                                        else:
+                                                                            print("incorrect")
+                                                                    else:
+                                                                        print("incorrect")
+                                                                else:
+                                                                    print("incorrect")
+                                                            else:
+                                                                print("incorrect")
+                                                        else:
+                                                            print("incorrect")
+                                                    else:
+                                                        print("incorrect")
+                                                else:
+                                                    print("incorrect")
+                                            else:
+                                                print("incorrect")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")                                                                                    
