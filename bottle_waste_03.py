c0307cd30edb929ce9acb8f626e3d7a9017e4b29
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-11 16:15:14 +0330

    ring area defects(surface cracks on the ring)

---
+++ bottle_waste_03.py
@@ -0,0 +1,88 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair the surface cracks on the ring below the branch area defects branch
+print("let's go to see what is surface cracks on the ring?")
+print("very small surface cracks, which are basically formed on the upper surface of the ring")
+# how to diagnose 
+print("how to diagnose?")
+print("it is difficult to detect this type of crack so that it does not reflect light well, so more care should be taken")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+melt = input("is the melt is very cold?   [yes/no]=>")
+if melt == "yes":
+    print("for fix it you have to measure the temperature of the cuticle")
+elif melt == "no":
+    print("okay answer this question")
+    # here is about operator and machion setting
+    print("here is about operator and machion setting")
+    plunger = input("is the plunger is very cold?   [yes/no]=>")
+    if plunger == "yes":
+        print("okay for fix it you have to reduce the cooling air of the plunger")
+    elif plunger == "no":
+        print("okay answer this question")
+        air = input("is there water in the air inflator mold?      [yes/no]=>")
+        if air == "yes":
+            print("for fix it you have to vacate path of air inflator mold")
+        elif air == "no":
+            print("okay answer this question")
+            lubrication = input("Is too much oil sprayed on the plunger or too much lubrication of the rings and blinks?     [yes/no]=>")
+            if lubrication == "yes":
+                print("for fix it you have to reduce lubrication")
+            elif lubrication == "no":
+                print("answer this question")
+                pipe = input("does it cause the upper edge of the rim to cool when the internal cooling pipe is not used in the mold inflatorer, or when the inflatorer hole is too large?   [yes/no]=>") 
+                if pipe == "yes":
+                    print("for fix you have to use a tube to inflate the mold with a smaller hole")
+                elif pipe == "no":
+                    print("okay answer this question")
+                    vacuum = input("is the time and actions of vacuum is long?   [yes/no]=>")
+                    if vacuum == "yes":
+                        print("for fix it reduce vacuum time")
+                    elif vacuum == "no":
+                        print("answer this question")
+                        pliers = input("is the jaws of the pliers are cold?   [yes/no]=>")
+                        if pliers == "yes":
+                            print("for fix you have to the jaws of the pliers are cold")
+                        elif pliers == "no":
+                            print("okay answer this question")
+                            # here is about template components
+                            print("here is about template components")
+                            inflator_the_depth = input("is the depth of the inflator mold is too low or too high?    [yes/no]=>") 
+                            if inflator_the_depth == "yes":
+                                print("for fix it you have to correct the inflator mold inflator depth")
+                            elif inflator_the_depth == "no":
+                                print("okay answer this questio")
+                                pliers2 = input("is the jaws of the pliers are very stiff?     [yes/no]=>")
+                                if pliers2 == "yes":
+                                    print("for fix it you have to check the pliers jaws for proper looseness")
+                                elif pliers2 == "no":
+                                    print("answer the last question")
+                                    ring = input("is the edge of the guide ring or the ring itself too sharp and not polished?    [yes/no]=>")
+                                    if ring == "yes":
+                                        print("for fix it check the tip ring form and pay for it")
+                                    elif ring == "no":
+                                        print("you say all the question no ):")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
