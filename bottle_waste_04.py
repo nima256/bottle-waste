d950a9ce6acc34c925fc879bbc340b0b6d526a17
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-12 15:31:19 +0330

    repair a ring area defects(crack under ring)

---
+++ bottle_waste_04.py
@@ -0,0 +1,208 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair crack under ring below the branch area defects branch
+print("let's go to see what is crack under ring")
+print("surface cracks between the separation line of the ring and the crack are called cracks under the ring")
+print("how to inspect and diagnose?")
+print("it can be detected by turning the bottle and reflecting light")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+heat = input("is the heat of the piece inappropriate?   [yes/no]=>")
+if heat == "yes":
+    print("for fix it you have to control the temperature of the part and adjust it if necessary")
+elif heat == "no":
+    print("okay answer this question")
+    # here is about operator and machion setting
+    print("here is about operator and machion setting")
+    mold = input("does the mold close too soon or too fast?   [yes/no]=>")
+    if mold == "yes":
+        print("for fix it you have to adjust the template closing operation")
+    elif mold == "no":
+        print("okay answer this question")
+        mold_holder = input("is mold holders are very worn?   [yes/no]=>") 
+        if mold_holder == "yes":
+            print("for fix it control the dimensions of the molds according to the information and specifications")
+        elif mold_holder == "no":
+            print("answer this question")
+            ring_arms = input("are the rining arms bent?   [yes/no]=>")
+            if ring_arms == "yes":
+                print("well for fix it you have to control the size of the ring holder to match the dimensional drawings")
+            elif ring_arms == "no":
+                print("okay answer this question")
+                ring_arms2 = input("aren't the arms of the rim pair paired and not the gonio?   [yes/no]=>")
+                if ring_arms2 == "yes":
+                    print("for fix it you have to control the invert mechanism with the corresponding fixture")
+                elif ring_arms2 == "no":
+                    print("okay answer this question")
+                    pliers_jaws = input("is pliers jaws close very quickly?  [yes/no]=>")
+                    if pliers_jaws == "yes":
+                        print("for fix it you have to delay the closing of the pliers jaws")
+                    elif pliers_jaws == "no":
+                        print("okay answer this question")
+                        pliers_jaws2 = input("is the jaws of the pliers are soaked in oil?    [yes/no]=>")
+                        if pliers_jaws2 == "yes":
+                            print("for fix it you have to clean the jaws of the pliers from any grease")
+                        elif pliers_jaws2 == "no":
+                            print("okay answer this question")
+                            pliers_jaws3 = input("aren't the pliers jaws adjusted?  [yes/no]=>")
+                            if pliers_jaws3 == "yes":
+                                print("for fix it you have to adjust and pair the pliers jaws")
+                            elif pliers_jaws3 == "no":
+                                print("okay answer this question")
+                                mold2 = input("doesn't the half-construction go down inside the mold, so that the mold inflator pushes the rim down?   [yes/no]=>")
+                                if mold2 == "yes":
+                                    print("well for fix it you have to check the neck of the mold for the correct shape and size")
+                                elif mold2 == "no":
+                                    print("okay answer another question")
+                                    links = input("does the wear and tear links of the blank holder cause one side of the blank to come out of the center and press against the ring when the blank is opened?   [yes/no]=>")
+                                    if links == "yes":
+                                        print("for fix it you have to replace new links")
+                                    elif links == "no":
+                                        print("okay answer this question")
+                                        ring = input("are the ring hold on the mold adjusted so low ، So that of the side of the blank and when the blank open it will Pressure to the ring?   [yes/no]=>")
+                                        if ring == "yes":
+                                            print("for fix it check the rims and adjust it with the relevant part if necessary")
+                                        elif ring == "no":
+                                            print("okay answer this question")
+                                            ring2 = input("is the ring opens too fast?   [yes/no]=>")
+                                            if ring2 == "yes":
+                                                print("for fix it you have to adjust the opening of the ring to move more slowly ")
+                                            elif ring2 == "no":
+                                                print("okay answer another question")
+                                                plunger = input("Is the plunger mechanism too high or too low? If the plunger is too high, it will pull over ring up and when it's open slide in the opposite direction when the blank is opened, and if it is too low, it will lift the blank when it is opened and closed.   [yes/no]=>")
+                                                if plunger == "yes":
+                                                    print("well for fix it you have adjust the plunger mechanism properly and at the required height so that the blank holder do not move when the ring and blank is opened and closed.")
+                                                elif plunger == "no":
+                                                    print("okay answer this question")
+                                                    mold_floor = input("is the mold floor is set too high or too low?   [yes/no]=>")
+                                                    if mold_floor == "yes":
+                                                        print("for fix it you have to adjust the mold floor mechanism to the correct height")
+                                                    elif mold_floor == "no":    
+                                                        print("okay answer this question")
+                                                        nozzles = input("is there a lot of cooling air on the nozzles?    [yes/no]=>")
+                                                        if nozzles == "yes":
+                                                            print("for fix it you have to reduce the cooling air of the mold and control the cooling settings")
+                                                        elif nozzles == "no":
+                                                            print("okay answer this question")
+                                                            lubrication = input("is the aghlam lubrication is a weak and irregular mold?   [yes/no]=>")
+                                                            if lubrication == "yes":
+                                                                print("for fix it you have to lubricate regularly with a well-shaped, clean and dry brush")
+                                                            elif lubrication == "no":
+                                                                print("okay answer this question")
+                                                                # here is about template components
+                                                                print("here is about template components")
+                                                                blank = input("is the diameter of the blank neck larger than the diameter of the mold neck?   [yes/no]=>")
+                                                                if blank == "yes":
+                                                                    print("for fix it you have to make sure that the mold neck diameter is larger than the blank blank diameter")
+                                                                elif blank == "no":
+                                                                    print("well you have to answer this question")
+                                                                    pliers_jaws4 = input("is the pliers jaws are defective or lack proper design and material?   [yes/no]=>")
+                                                                    if pliers_jaws4 == "yes":
+                                                                        print("for fix it you have to make sure the pliers jaw is loose correctly")
+                                                                    elif pliers_jaws4 == "no":
+                                                                        print("okay answer another question")
+                                                                        pliers_jaws5 = input("is the diameter of the pliers jaws is small?   [yes/no]=>")
+                                                                        if pliers_jaws5 == "yes":
+                                                                            print("for fix it you have to make sure the pliers jaw is loose correctly")
+                                                                        elif pliers_jaws5 == "no":
+                                                                            print("okay amswer this question")
+                                                                            pliers_jaws6 = input("is the jaws of the pliers do not open properly?   [yes/no]=>")
+                                                                            if pliers_jaws6 == "yes":
+                                                                                print("for fix it you have to easily return pliers jaws")
+                                                                            elif pliers_jaws6 == "no":
+                                                                                print("okay answer this question")
+                                                                                ring3 = input("is the surface of the ring is worn?   [yes/no]=>")
+                                                                                if ring3 == "yes":
+                                                                                    print("for fix it you have to use the new tip ring")
+                                                                                elif ring3 == "no":
+                                                                                    print("answer another question")
+                                                                                    sholder = input("is the radius of curvature of the shoulder too small and sharp?   [yes/no]=>")
+                                                                                    if sholder == "yes":
+                                                                                        print("for fix it you have to check the mold arch for release")
+                                                                                    elif sholder == "no":
+                                                                                        print("okay answer this question")
+                                                                                        mold3 = input("is the bottom of the mold is very hard in the mold?   [yes/no]=>")                                        
+                                                                                        if mold3 == "yes":
+                                                                                            print("for fix it you have to use mold gauges to measure mold equipment")
+                                                                                        elif mold3 == "no":
+                                                                                            print("okay answer this question")
+                                                                                            mold4 = input("is there no arched mold at the top?   [yes/no]=>")
+                                                                                            if mold4 == "yes":
+                                                                                                print("for fix it you have to make sure there is an arch at the top of the mold")
+                                                                                            elif mold4 == "no":
+                                                                                                print("okay answer this question")
+                                                                                                ring4 = input("is the crown of the ring is very sharp?   [yes/no]=>")
+                                                                                                if ring4 == "yes":
+                                                                                                    print("for fix it you have to expand the ring crown arch ")
+                                                                                                elif ring4 == "no":
+                                                                                                    print("okay snwer this question")
+                                                                                                    # here is about stacker equipment
+                                                                                                    print("here is about stacker equipment") 
+                                                                                                    jaws_and_fingers = input("are the settings on the jaws or fingers of the stacker paddle receiver incorrect?    [yes/no]=>")
+                                                                                                    if jaws_and_fingers == "yes":
+                                                                                                        print("for fix itcheck that the receiver's jaws and fingers are properly adjusted")
+                                                                                                    elif jaws_and_fingers == "no":
+                                                                                                        print("answer the last question")
+                                                                                                        stacker_paddle = input("does the stacker paddle lack proper insulation material or is it not selected from the correct material?   [yes/no]=>")
+                                                                                                        if stacker_paddle == "yes":
+                                                                                                            print("for fix it you have to if necessary, use fireproof insulation materials in the stacker paddle or fingers")
+                                                                                                        elif stacker_paddle == "no":
+                                                                                                            print("you say all the question no ): ")
+                                                                                                        else:
+                                                                                                            print("incorrect")
+                                                                                                    else:
+                                                                                                        print("incorrect")
+                                                                                                else:
+                                                                                                    print("incorrect")
+                                                                                            else:
+                                                                                                print("incorrect")
+                                                                                        else:
+                                                                                            print("incorrecct")
+                                                                                    else:
+                                                                                        print("incorrect")
+                                                                                else:
+                                                                                    print("incorrect")
+                                                                            else:
+                                                                                print("incorrect")
+                                                                        else:
+                                                                            print("incorrect")
+                                                                    else:
+                                                                        print("incorrect")
+                                                                else:
+                                                                    print("incorrect")
+                                                            else:
+                                                                print("incorrect")
+                                                        else:
+                                                            print("incorrect")
+                                                    else:
+                                                        print("incorrect")
+                                                else:
+                                                    print('incorrect')
+                                            else:
+                                                print("incorrect")
+                                        else:
+                                            print("incorreect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
