4adc63bf6eed1d062a38db0521ed196ffc785654
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-15 11:36:16 +0330

    repair a ring area defects(narrow throat(in narrow-mouth bottles))

---
+++ bottle_waste_06.py
@@ -0,0 +1,105 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair narrow throat(in narrow-mouth bottles) below the branch area defects branch
+
+
+
+print("let's go to see what is narrow throat(in narrow-mouth bottles)")
+print("cramps in the inside of the neck are called")
+print("attention : bottles are usually filled with filling tubes in industry. If the inside of the neck is too small, the filling tube gets stuck in it, or breaks itself, or the bottle breaks, which stops the filling line and production.")
+print("how to inspect and diagnose") 
+print("all narrow neck bottles should be tested and controlled with the correct gauge")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+piece = input("is the temperature of the molten glass non-uniform and usually very hot?  [yes/no]=>")
+if piece == "yes":
+    print("for fix it have to adjust the temperature of the forhath so that the piece cannot bend")
+elif piece == "no":
+    print("okay answer this question")
+    piece2 = input("is the piece is very round and short?   [yes/no]=>")
+    if piece2 == "yes":
+        print("for fix it you have to adjust the shape of the piece")
+    elif piece2 == "no":
+        print("answer this question")
+        # here is about operator and machion setting
+        print("here is about operator and machion setting")
+        plunger = input("is the air inflator of the plunger is early so the plunger does not have the opportunity to reheat?  [yes/no]=>")
+        if plunger == "yes":
+            print("for fix it you have to delay the start of the air inflator of the plunger or counter blue")
+        elif plunger == "no":
+            print("okay answer this question")
+            plunger2 = input("is it too late to start airing the plunger or counter blue, so the chance of the plunger heating up is very high and the glass is very hot?  [yes/no]=>")
+            if plunger2 == "yes":
+                print("for fix it you have to push the starter air inflator forward and cool the glass")
+            elif plunger2 == "no":
+                print("answer this question")
+                B_and_B = input("is the cooling air of the plunger is very high in B&B, so it causes the plunger to cool?  [yes/no]=>")
+                if B_and_B == "yes":
+                    print("for fix it you have to reduce the cooling air of the plunger")
+                elif B_and_B == "no":
+                    print("okay answer this question")
+                    plunger3 = input("is the contact time of the plunger is very long and causes the glass to cool?   [yes/no]=>")
+                    if plunger3 == "yes":
+                        print("for fix it reduce planger call time")
+                    elif plunger3 == "no":
+                        print("okay answer this question")
+                        lubrications = input("is the number of lubrications and also the amount of oil on the ring ring high?  [yes/no]=>")
+                        if lubrications == "yes":
+                            print("for fix it you have to install oil catchers on deflectors")
+                        elif lubrications == "no":
+                            print("okay answer this question")
+                            B_and_B2 = input("is blank cooling not uniform in systems B&B?   [yes/no]=>")
+                            if B_and_B2 == "yes":
+                                print("for fix it you have to match the cooling air on the two half blanks")
+                            elif B_and_B2 == "no":
+                                print("okay answer this question")
+                                blank = input("are the blanks hot and dry?   [yes/no]=>")
+                                if blank == "yes":
+                                    print("for fix it you have to increase the cooling air of the blank and lubricate it if necessary")
+                                elif blank == "no":
+                                    print("okay answer this question")
+                                    ring__or_plunger = input("is the ring or plunger dirty?   [yes/no]=>")
+                                    if ring__or_plunger == "yes":
+                                        print("for fix it replace the ring or plunger")
+                                    elif ring__or_plunger == "no":
+                                        print("answer this question")
+                                        # here is about template components
+                                        print("here is about template components")
+                                        plunger4 = input("is the planner design incorrect?    [yes/no]=>")
+                                        if plunger4 == "yes":
+                                            print("for fix it you have to modify the planner design")
+                                        elif plunger4 == "no":
+                                            print("answer the last question")
+                                            pliers_jaws = input("is the jaws of the pliers are very small?    [yes/no]=>") 
+                                            if pliers_jaws == "yes":
+                                                print("for fix it correct the size of the pliers jaws")
+                                            elif pliers_jaws == "no":
+                                                print("you say all the question no ): ")
+                                            else:
+                                                print("incorrect")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorret")
+else:
+    print("incorrect")
