6d297812acd8c0f63721e207e714d59dbb493141
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-15 11:32:50 +0330

    repair a ring area defects(a little ring edge)

---
+++ bottle_waste_05.py
@@ -0,0 +1,172 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair a little ring edge below the branch area defects branch
+
+
+print("let's go to see what is a little ring edge")
+print("a ring whose edge, thread or strake is not completely filled is called")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+mold = input("is the melt temperature is low?   [yes/no]=>")
+if mold == "yes":
+    print("for fix it you have to adjust the glass temperature")
+elif mold == "no":
+    print("answer this question")
+    P_and_B = input("In the P&B system, the weight of the piece is low")
+    if P_and_B == "yes":
+        print("for fix it you have to increase the weight")
+    elif P_and_B == "no":
+        print("well answer this question")
+        piece = input("is the shape of the piece is not appropriate?  [yes/no]=>")
+        if piece == "yes":
+            print("for fix it you have to correct the shape of the piece")
+        elif piece == "no":
+            print("well answer this question")
+            # here is about operator and machion setting
+            print("here is about operator and machion setting")
+            blank = input("is the blank inflator time or air pressure is not enough?  [yes/no]=>")
+            if blank == "yes":
+                print("for fix it you have to Increase the air of the blank inflator")
+            elif blank == "no":
+                print("okay answer this question")
+                B_and_B = input("are the baffle holes blocked with glass in B&B?  [yes/no]=>")
+                if B_and_B == "yes":
+                    print("for fix it you have to change the baffle")
+                elif B_and_B == "no":
+                    print("well you have to answer this question")
+                    piece2 = input("is it improper to guide the piece to the blank or does the piece get stuck in the studs?   [yes/no]=>")
+                    if piece2 == "yes":
+                        print("do you guide the part to make it more suitable by using oil or smaller bending studs?   [yes/no]=>")
+                    elif piece2 == "no":
+                        print("well answer this question")
+                        studs = input("is the adjustment of the bending studs not correct?   [yes/no]=>")
+                        if studs == "yes":
+                            print("for fix it you have to align the studs with the blank")
+                        elif studs == "no":
+                            print("okay answer this question")
+                            P_and_B2 = input("is the pressure of the plunger press in the P&B system is low   [yes/no]=>")
+                            if P_and_B2 == "yes":
+                                print("for fix it you have to Increase press time or pressure")
+                            elif P_and_B2 == "no":
+                                print("okay answer this question")
+                                P_and_B3 = input("is the ring, guide or plunger in the P&B system is dirty?  [yes/no]=>")
+                                if P_and_B3 == "yes":
+                                    print("for fix it you have to replace the ring and plunger")
+                                elif P_and_B3 == "no":
+                                    print("well you have to answer this question")
+                                    B_and_B2 = input("is the air of the plunger inflator in system B&B is blown quickly?   [yes/no]=>")
+                                    if B_and_B2 == "yes":
+                                        print("for fix it you have to delay the start time of the Planger inflator")
+                                    elif B_and_B2 == "no":
+                                        print("okay answer this question")
+                                        lubrication = input("is there insufficient lubrication of the ring, blank and plunger?   [yes/no]=>")
+                                        if lubrication == "yes":
+                                            print("for fix it you have to lubricate the blanks more frequently and adjust the oil sprays")
+                                        elif lubrication == "no":
+                                            print("well answer this question")
+                                            ring_components = input("is the gas trapped inside the blank in such a way that it does not allow the piece to enter the ring?    [yes/no]=>") 
+                                            if ring_components == "yes":
+                                                print("for fix it you have to increase the possibility of venting air in the ring components")
+                                            elif ring_components == "no":
+                                                print("okay answer this question")
+                                                vacuum = input("is it not enough to announce the vacuum time on the blank side?  [yes/no]=>")
+                                                if vacuum == "yes":
+                                                    print("for fix it you have to increase the vacuum time")
+                                                elif vacuum == "no":
+                                                    print("answer this question")
+                                                    baffle = input("is the baffle, funnel and related arms adjusted correctly?   [yes/no]=>")
+                                                    if baffle == "yes":
+                                                        print("for fix it adjust the sitting and take-off time of the baffle and funnel and re-measure the relevant arms on the blank")
+                                                    elif baffle == "no":
+                                                        print("answer this question")
+                                                        ring = input("is the nick ring is very hot?    [yes/no]=>")
+                                                        if ring == "yes":
+                                                            print("for fix it use ring cooling nozzles")
+                                                        elif ring == "no":
+                                                            print("well answer this question")
+                                                            P_and_B4 = input("is the height of the Palanger mechanism low in P&B?   [yes/no]=>")
+                                                            if P_and_B4 == "yes":
+                                                                print("for fix it adjust the height of the plunger mechanism so that it is in the proper position for the piece to fall into the blank")
+                                                            elif P_and_B4 == "no":
+                                                                print("answer this question")
+                                                                blank2 = input("are the blanks hot and the peice do not fit easily inside the blanks?   [yes/no]=>")
+                                                                if blank2 == "yes":
+                                                                    print("for fix it you have to reduce the blank temperature and modify the cooling and lubrication method")
+                                                                elif blank2 == "no":
+                                                                    print("answer this question")
+                                                                    # here is about template components
+                                                                    print("here is about template components")
+                                                                    ring2 = input("is not enough air venting in the ring components?   [yes/no]=>")
+                                                                    if ring2 == "yes":
+                                                                        print("for fix it you have to correct air discharge in ring components")
+                                                                    elif ring2 == "no":
+                                                                        print("well answer this question")
+                                                                        ring3 = input("are the ring components dirty?  [yes/no]=>")
+                                                                        if ring3 == "yes":
+                                                                            print("for fix it replace the nick ring")
+                                                                        elif ring3 == "no":
+                                                                            print("answer this question")
+                                                                            B_and_B3 = input("is the ring equipment is not suitable in systems B&B?  [yes/no]=>")
+                                                                            if B_and_B3 == "yes":
+                                                                                print("for fix it change the size of the nick ring")
+                                                                            elif B_and_B3 == "no":
+                                                                                print("answer this question")
+                                                                                P_and_B5 = input("is the semi-finished design in P&B is not suitable?  [yes/no]=>")
+                                                                                if P_and_B5 == "yes":
+                                                                                    print("for fix it you have to use plungers and blanks with appropriate design")
+                                                                                elif P_and_B5 == "no":
+                                                                                    print("okay answer the last question")
+                                                                                    funnel = input("is the size of the funnel is small?   [yes/no]=>")
+                                                                                    if funnel == "yes":
+                                                                                        print("for fix it you have to check the size of the funnel")
+                                                                                        print("attention : After the blanks are worn, the radii will change with the radius of the rings. You have to match them")
+                                                                                    elif funnel == "no":
+                                                                                            print("you say all thing no ): ")
+                                                                                    else:
+                                                                                        print("incorrect")
+                                                                                else:
+                                                                                    print("incorrect")
+                                                                            else:
+                                                                                print("incorrect")
+                                                                        else:
+                                                                            print("incorrect")
+                                                                    else:
+                                                                        print("incorrect")
+                                                                else:
+                                                                    print("incorrect")
+                                                            else:
+                                                                print("incorrect")
+                                                        else:
+                                                            print("incorrect")
+                                                    else:
+                                                        print("incorrect")
+                                                else:
+                                                    print("incorrect")
+                                            else:
+                                                print("incorrect")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
