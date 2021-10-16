abc0a89dc938f49088eb68b9e560da59e32fb01e
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-16 10:22:48 +0330

    repair a ring area defects(ring displacement)

---
+++ bottle_waste_07.py
@@ -0,0 +1,100 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair ring displacement below the branch area defects branch
+
+
+
+print("let's go to see what is ring displacement")
+print("refers to a ring that is out of its axis in a horizontal or vertical direction")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+piece = input("is the glass too hot?   [yes/no]=>")
+if piece == "yes":
+    print("for fix it you have to adjust the piece temperature")
+elif piece == "no":
+    print("okay answer this question")
+    # here is about operator and machion setting
+    print("here is about operator and machion setting")
+    ring = input("is the ring on the mold out of center?   [yes/no]=>")
+    if ring == "yes":
+        print("for fix it you have to re-adjust the ring gripping arms on the mold using the appropriate gauge")
+    elif ring == "no":
+        print("okay answer this question")
+        ring2 = input("are the ring arms not perfectly aligned?   [yes/no]=>")
+        if ring2 == "yes":
+            print("for fix it you have control the invert mechanism using the corresponding fixture")
+        elif ring2 == "no":
+            print("okay answer this question")
+            ring_returner = input("has the impact of transferring the semi-finished product by ring returner the mold not been done?   [yes/no]=>")
+            if ring_returner == "yes":
+                print("for fix it control the speed and impact of the ring returner")
+            elif ring_returner == "no":
+                print("answer this question")
+                cool_the_air = input("are the cooling settings incorrect so that when it does not cool enough the ring will be warm?    [yes/no]=>")
+                if cool_the_air == "yes":
+                    print("for fix it you have to cool the air and adjust its settings")
+                elif cool_the_air == "no":
+                    print("okay answer this question")
+                    ring_springs = input("are the springs of the ring arms removed?  [yes/no]=>")
+                    if ring_springs == "yes":
+                        print("for fix it you have to check the opening of the ring (replace the rim if necessar)")
+                    elif ring_springs == "no":
+                        print("okay answer this question")
+                    ring_returner2 = input("is there a lack of proper timing between the turning ring and the closing of the mold?   [yes/no]=>")
+                    if ring_returner2 == "yes":
+                        print("for fix it you have to give proper time between closing the mold and the ring")
+                    elif ring_returner2 == "no":
+                        print("okay answer this question")
+                        # here is about template components
+                        print("here is about template components")
+                        tab_ring = input("is the tab ring (ring living room on the blank) small?   [yes/no]=>") 
+                        if tab_ring == "yes":
+                            print("for fix it you have to control it with the right gauge")
+                        elif tab_ring == "no":
+                            print("okay answer this question")
+                            blank_living_room = input("is blank living room(the bottom, where the ring is located) too big?   [yes/no]=>")
+                            if blank_living_room == "yes":
+                                print("for fix it you have to check its dimensions based on the template design drawings")
+                            elif blank_living_room == "no":
+                                print("okay answer this question")
+                                mold = input("is the neck of the mold widened?  [yes/no]=>")
+                                if mold == "yes":
+                                    print("for fix it you have to change the mold")
+                                elif mold == "no":
+                                    print("okay answer this question")
+                                    nick_ring = input("is the guid ring worn and old?  [yes/no]=>")
+                                    if nick_ring == "yes":
+                                        print("for fix it replace the nick ring")
+                                    elif nick_ring == "no":
+                                        print("okay answer the last question")
+                                        mold2 = input("is the mold inflator arm is not centered or adjusted?   [yes/no]=>")
+                                        if mold2 == "yes":
+                                            print("for fix it you have to align the mold inflator arm with the mold")
+                                        elif mold2 == "no":
+                                            print("you say all the question no ):")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
