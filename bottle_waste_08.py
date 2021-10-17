a63f9d7ada83b32189f10f6e277280a33c40d621
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-17 16:23:45 +0330

    repair a ring area defects(swelling of the ring)

---
+++ bottle_waste_08.py
@@ -0,0 +1,96 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair swelling of the ring below the branch area defects branch
+
+
+
+print("let's go to see what is swelling of the ring")
+print("a ring that is inflated by air pressure or mechanical swelling")
+print("Attention : this defect prevents the good lid of the container will cause its contents to be lost and corrupted")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+melt = input("is the molten glass is very hot?   [yes/no]=>")
+if melt == "yes":
+    print("for fix it you have to measure the piece temperature")
+elif melt == "no":
+    print("answer this question")
+    peice = input("is the piece form incorrect?   [yes/no]=>")
+    if peice == "yes":
+        print("for fix it correct the shape of the piece")
+    elif peice == "no":
+        print("okay answer this question")
+        # here is about operator and machion setting
+        print("here is about operator and machion setting")
+        ring = input("is the ring is not very firm and still soft due to insufficient time or air pressure of the blank inflator or dry blank, which reduces the contact between the glass and the rim components?     [yes/no]=>")
+        if ring == "yes":
+            print("for fix it you have to Increase the time and air inflator of the blank and lubricate the blank")
+        elif ring == "no":
+            print("okay answer this question")
+            plunger = input("is planger call time too short?    [yes/no]=>")
+            if plunger == "yes":
+                print("for fix it you have to increase the contact time of the plunger")
+            elif plunger == "no":
+                print("okay answer this question")
+                ring2 = input("is the nick ring very hot?   [yes/no]=>")
+                if ring2 == "yes":
+                    print("for fix it you have to Use the ring cooling nozzle")
+                elif ring2 == "no":
+                    print("okay answer this question")
+                    internal_cooling_and_air_exhaust = input("is the internal cooling pipe too big or the air exhaust hole too small?    [yes/no]=>")
+                    if internal_cooling_and_air_exhaust == "yes":
+                        print("for fix it you have to use a smaller tube in the mold balloon and enlarge the exhaust holes")
+                    elif internal_cooling_and_air_exhaust == "no":
+                        print("okay answer this question")
+                        mold_inflator = input("is the mold inflator out of alignment and center?   [yes/no]=>")
+                        if mold_inflator == "yes":
+                            print("for fix it you have to adjust the mold inflator on the mold again")
+                        elif mold_inflator == "no":
+                            print("okay answer this question")
+                            counter_blue = input("is the counter blue air not enough?   [yes/no]=>")
+                            if counter_blue == "yes":
+                                print("for fix it you have to increase the counter blue air")
+                            elif counter_blue == "no":
+                                print("okay answer this question")
+                                # here is about template components
+                                print("here is about template components")
+                                mold_inflator2 = input("is the depth of the mold inflator is shallow so that it contacts the ring and compresses it?    [yes/no]=>")
+                                if mold_inflator2 == "yes":
+                                    print("for fix it you have to increase the depth of the mold inflator")
+                                elif mold_inflator2 == "no":
+                                    print("okay answer this question")
+                                    mold_inflator3 = input("are the mold holes blicked?   [yes/no]=>")
+                                    if mold_inflator3 == "yes":
+                                        print("for fix it you have to open the air vent holes in the mold inflator")
+                                    elif mold_inflator3 == "no":
+                                        print("answer the last question")
+                                        neck_of_mold = input("is the diameter of the neck of the mold less than half the construction, so that the construction does not fit well in the mold and remains a little high, so the inflator presses on the ring?   [yes/no]=>")
+                                        if neck_of_mold == "yes":
+                                            print("for fix it check the neck of the mold for size")
+                                        elif neck_of_mold == "no":
+                                            print("you say all the question no ):")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
