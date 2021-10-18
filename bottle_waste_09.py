46f189ed20daa29b8f47a2fe061bed28c06f2e01
nimamahdi <www.nimamahdi1385@gmail.com>
2021-10-18 21:42:59 +0330

    repair a ring area defects(swelling of the ring)

---
+++ bottle_waste_09.py
@@ -0,0 +1,132 @@
+#!/bin/python3
+
+# hello
+# here we want to talk about repair bottle waste
+# here in this script the title of repaire is repair a ring area defects
+# ring area defects is tittle and we have lots of different problem under it
+# we repair ring paleness below the branch area defects branch
+
+
+
+print("let's go to see what is ring paleness")
+print("a ring is a small part of the edge or armpit that is broken")
+# here the title of problem is about feeder
+print("here the title of problem is about feeder")
+feeder = input("is the glass too cold?    [yes/no]=>")
+if feeder == "yes":
+    print("for fix it you have to adjust the feeder temperature")
+elif feeder == "no":
+    print("okay answer this question")
+    # here is about operator and machion setting
+    print("here is about operator and machion setting")
+    ring = input("does the ring holder close quickly when returning to the blank and cause the rim to be compressed?    [yes/no]=>")
+    if ring == "yes":
+        print("for fix it you have to delay the closing time of the ring")
+    elif ring == "no":
+        print("okay answer this question")
+        ring2 = input("is the opening of the ring on the mold either not enough or the amount of opening on both sides is not the same?      [yes/no]=>")
+        if ring2 == "yes":
+            print("increase the opening value of the ring(and equalize the opening value on both sides)")
+        elif ring2 == "no":
+            print("okay answer this question")
+            bumper_mechanism = input("does the inverter mechanism or inverter ring not work well when moving towards the mold?   [yes/no]=>")
+            if bumper_mechanism == "yes":
+                print("for fix it adjust the bumper mechanism")
+            elif bumper_mechanism == "no":
+                print("well answer this question")
+                ring3 = input("is the opening of the ring severe?   [yes/no]=>")
+                if ring3 == "yes":
+                    print("for fix it you have to adjust the opening speed of the ring for a smoother movement")
+                elif ring3 == "no":
+                    print("okay answer this question")
+                    mold = input("is the mold inflator adjustment incorrect and out of center?   [yes/no]=>")
+                    if mold == "yes":
+                        print("for fix it you have to adjust the mold inflator on the mold correctly")
+                    elif mold == "no":
+                        print("okay answer this question")
+                        shock_absorber = input("is the shock absorber at the end of the course of the pliers mechanism (moving from the mold to the conveyor) not working properly?   [yes/no]=>")
+                        if shock_absorber == "yes":
+                            print("for fix it you have to adjust the end-of-course shock absorber of the pliers mechanism")
+                        elif shock_absorber == "no":
+                            print("okay answer this question")
+                            pliers_jaws = input("are the jaws of the pliers not level and in the center and are they tightly closed?      [yes/no]=>") 
+                            if pliers_jaws == "yes":
+                                print("for fix it adjust the jaws of the pliers and make it easier to close")
+                            elif pliers_jaws == "no":
+                                print("okay answer this question")
+                                ring4 = input("is the setting the ring holder on the mold is not correct?   [yes/no]=>")
+                                if ring4 == "yes":
+                                    print("for fix it you have to adjust the ring on the mold with the ring adjusting gauge")
+                                elif ring4 == "no":
+                                    print("okay answer this question")
+                                    mold_floor = input("is the height of the mold floor high?   [yes/no]=>")
+                                    if mold_floor == "yes":
+                                        print("for fix it you have adjust the height of the mold floor")
+                                    elif mold_floor == "no":
+                                        print("okay answer this question")
+                                        # here is about template components
+                                        print("here is about template components")
+                                        mold2 = input("is the bottom of the mold is firm (makes the mold difficult to open)?   [yes/no]=>")
+                                        if mold2 == "yes":
+                                            print("for fix it you have to check the mold components with the corresponding gauges")
+                                        elif mold2 == "no":
+                                            print("okay asnswer this question")
+                                            batten_bottles = input("is the radius of the batten arc in batten bottles very small and sharp, and due to the shaking of the bottle on the bottom of the mold when closing the pliers jaws, the jaw collides with the batten and its paleness?    [yes/no]")
+                                            if batten_bottles == "yes":
+                                                print("for fix it make sure the batten radius is slightly wide and released easily")
+                                            elif batten_bottles == "no":
+                                                print("okay answer this question")
+                                                # here is about stacker equipment 
+                                                print("here is about stacker equipment")
+                                                stacker_paddle = input("is it not appropriate to adjust the stacker paddle?    [yes/no]=>") 
+                                                if stacker_paddle == "yes":
+                                                    print("for fix it you have to adjust the stacker paddle")
+                                                elif stacker_paddle == "no":
+                                                    print("okay answer this question")
+                                                    stacker_paddle2 = input("is the stacker paddle operation time correct?    [yes/no]=>")
+                                                    if stacker_paddle2 == "yes":
+                                                        print("for fix it adjust the setting and movement time of the stacker paddle")
+                                                    elif stacker_paddle2 == "no":
+                                                        print("okay answer this question")
+                                                        stacker_paddle3 = input("is the product not placed correctly on the cross conveyor and the stacker paddle hits the ring?    [yes/no]=>")
+                                                        if stacker_paddle3 == "yes":
+                                                            print("for fix it you have to correct the position of the product on the cross conveyor")        
+                                                        elif stacker_paddle3 == "no":
+                                                            print("okay answer the last question")
+                                                        # here is about inspection equipment  
+                                                        print("here is about inspection equipment")    
+                                                        inspection_device = input("are the inspection device settings(such as the head gauge)not right?    [yes/no]=>")
+                                                        if inspection_device == "yes":
+                                                            print("for fix it modify the inspection device settings in the quality control section")
+                                                        elif inspection_device == "no":
+                                                            print("you say all the question no ):")
+                                                        else:
+                                                            print("incorrect")
+                                                    else:
+                                                        print("incorrect")
+                                                else:
+                                                    print("inocorrect")
+                                            else:
+                                                print("incorrect")
+                                        else:
+                                            print("incorrect")
+                                    else:
+                                        print("incorrect")
+                                else:
+                                    print("incorrect")
+                            else:
+                                print("incorrect")
+                        else:
+                            print("incorrect")
+                    else:
+                        print("incorrect")
+                else:
+                    print("incorrect")
+            else:
+                print("incorrect")
+        else:
+            print("incorrect")
+    else:
+        print("incorrect")
+else:
+    print("incorrect")
